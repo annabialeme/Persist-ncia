fb63720fcfbb67d62ddfabccdd9f139e6fd0085b
anna leme <annaleme.tds1.senai@gmail.com>
2025-04-23 14:52:02 -0300

    add: css detalhes

--- pages/Detalhes.js
+++ pages/Detalhes.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from react;
-import { View, Text} from "react-native";
+import { View, Text, StyleSheet} from "react-native";
 import * as SecureStore from "expo-secure-store";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import Entypo from '@expo/vector-icons/Entypo';
@@ -40,3 +40,39 @@ return (
 
     </View>
 )
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "f5f5f5",
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "333",
+    },
+    subtitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginTop: 15,
+        marginBottom: 10,
+    },
+    peopleContainer: {
+        alignItems: "center",
+        marginBottom: 20,
+    },
+    personText: {
+        fontSize: 16,
+        color: "#444",
+        marginVertical: 5,
+    },
+    iconsContainer: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        width: "100%",
+        marginTop: 20,
+    },
+})
